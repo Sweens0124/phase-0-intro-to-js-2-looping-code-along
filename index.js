ed3912fa0453e62fe529c3b70441c038e8426598
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-19 14:04:02 -0600

    commit tree 6add6008177212b8d0608e05b6e9763d084b7dcc

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const newArr = []
+
+function writeCards (name, event) {
+  for (let i = 0; i < name.length; i++)
+    newArr.push(`Thank you, ${name[ i ]}, for the wonderful ${event} gift!`)
+  return newArr
+}
+
+function countDown () {
+  let count = 11
+  while (count > 0) {
+    console.log(--count)
+  }
+  return count
+}
+
